29b3f113108310ed87842080bedb7bb899604efd
Sheogorattio <no.passion.there.is.serenity@gmail.com>
2023-02-23 01:23:55 +0200

    Add project files.

---
+++ 10hw/Bag.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace BagClass
+{
+    public struct _Object
+    {
+        string name;
+        int volume;
+        int weight;
+
+        public _Object(string name, int volume, int weight)
+        {
+            this.name = name;
+            this.volume = volume;
+            this.weight = weight;
+        }
+    }
+
+    public delegate void AddItemDelegate(_Object newObj);
+
+    public class AddItemEventSource
+    {
+        public event AddItemDelegate ev;
+        public void GenerateEvent(_Object newObject)
+        {
+            ev(newObject);
+        }
+    }
+    public class Bag
+    {
+      
+        public string Color { get; set; }
+        public string Brand { get; set; }
+        private int totalWeight=0;
+        public int TotalWeight { get { return totalWeight; } }
+        private int totalVolume=100;
+        public int TotalVolume { get { return totalVolume; } }
+
+        _Object[] obj;
+        public void AddItem(_Object newObject)
+        {
+            _Object[] newArr = new _Object[obj.Length+1];
+            for(int i=0; i< obj.Length; i++)
+            {
+                newArr[i] = obj[i];
+            }
+            newArr[newArr.Length - 1]=newObject;
+        }
+    }
+}
---
+++ 10hw/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10hw
+{
+    delegate int CountInArr(int[] arr);
+    internal class Program
+    {
+
+        delegate int[] AnalyseRGB(string color);
+        static void Main(string[] args)
+        {
+            AnalyseRGB analyseRGB = delegate (string color)
+            {
+                Color col = Color.FromName(color);
+                int[] Code =new int[3] {col.R, col.G, col.B};
+                return Code;
+            };
+            foreach(var i in analyseRGB("blue"))
+            {
+                Console.Write("{0} ",i);
+            }
+            Console.WriteLine();
+
+            CountInArr CountDiv7 = arr =>
+            {
+                int count = 0;
+                for (int i=0; i < arr.Length; i++)
+                {
+                    if (arr[i] % 7 == 0) count++;
+                }
+                return count;
+            };
+
+            CountInArr CountPositive = arr =>
+            {
+                int count = 0;
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (arr[i] > 0) count++;
+                }
+                return count;
+            };
+
+            CountInArr CountUniqNegative = arr =>
+            {
+                int count = 0;
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (arr[i] < 0)
+                    {
+                        int repeats = 0;
+                        for(int j=i; j<arr.Length; j++)
+                        {
+                            if(arr[j] == arr[i]) repeats++;
+                        }
+                        if(repeats == 1) count++;
+                    }
+                }
+                return count;
+            };
+
+
+            Console.ReadKey();
+        }
+    }
+}
